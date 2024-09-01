c183c655bc5ce429777918111676e87d9e472963
SomeoneOnEarth <github@someone.earth>
2024-09-02 00:23:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Disable App / Window Switcher Popup Hover in Gnome 45+
+
+This Gnome Extension prevents the mouse cursor interfering with either the App Switcher or the Window Switcher popups.
+Note: It doesn't prevent mouse clicks.
+
+These are the switcher popups that are invoked from keyboard shortcuts, e.g. Alt+Tab or Super+Tab.
+
+Based on <https://extensions.gnome.org/extension/4714/disable-hover-on-app-window-switcher-popups/> (for Gnome <45)
+
---
+++ extension.js
@@ -0,0 +1,31 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as SwitcherPopup from 'resource:///org/gnome/shell/ui/switcherPopup.js';
+
+export default class DisableSwitcherPopupHover extends Extension {
+    enable() {
+        this._switcherPopupDisableHover = SwitcherPopup.SwitcherPopup.prototype._disableHover;
+        SwitcherPopup.SwitcherPopup.prototype._disableHover = () => {};
+    }
+
+    disable() {
+        SwitcherPopup.SwitcherPopup.prototype._disableHover = this._switcherPopupDisableHover;
+        this._switcherPopupDisableHover = null;
+    }
+}
